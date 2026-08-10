2b2418b4e2e1981fe41129c32cdba6ce119c0010
congliu-design <congliu@stanford.edu>
2026-08-10 14:23:07 -0700

    Update functions.py

--- vasq/vasq/functions.py
+++ vasq/vasq/functions.py
@@ -1788,7 +1788,7 @@ def build_matrix_expression_plot(stats_df, gene_order=None, region_order=None):
                 "x": 0.02,
                 "xanchor": "left",
             },
-            "height": max(440, 170 + 32 * len(gene_order)),
+            "height": max(400, 170 + 32 * len(gene_order)),
             "autosize": True,
             "paper_bgcolor": "rgba(0,0,0,0)",
             "plot_bgcolor": "#ffffff",
